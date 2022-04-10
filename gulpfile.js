be0772f2ff1912da936cdb5edf5f1b378c7df9c0
Denis <den-kir1@yandex.ru>
2022-04-11 00:01:05 +0300

    add: active slider

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,28 @@
 const gulp = require('gulp');
 const sass = require('sass');
 const gulpSass = require('gulp-sass');
+const babel = require('gulp-babel');
+const gulpConcat = require('gulp-concat');
 
 const scssCompiler = gulpSass(sass);
 
+
+gulp.task('js', () => {
+    return gulp.src('./src/**/*.js')
+        .pipe(babel({
+            presets: ['@babel/env'],
+        }))
+        .pipe(gulpConcat('index.js'))
+        .pipe(gulp.dest('./dest/js'));
+})
+
+
 gulp.task('styles', () => {
-    return gulp.src('./scss/**/*.scss')
+    return gulp.src('./src/scss/*.scss')
         .pipe(scssCompiler().on('error', scssCompiler.logError))
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./dest/css'))
 })
 
 gulp.task('watch', () => {
-    return gulp.watch('./scss/**/*.scss', gulp.series('styles'))
+    return gulp.watch('./src/**/*', gulp.series('styles', 'js'))
 });
---
+++ src/index.js
@@ -0,0 +1,58 @@
+const dots = document.getElementsByClassName('slider__tab--item');
+const dotsArea = document.getElementsByClassName('slider__tab')[0];
+const slides = document.getElementsByClassName('slider__item');
+var slideIndex = 1;
+const prevBtn = document.querySelector('.left-btn');
+const nextBtn = document.querySelector('.rigth-btn');
+
+
+showSlides(slideIndex);
+
+function showSlides(n) {
+    if (n < 1) {
+        slideIndex = slides.length;
+    } else if (n > slides.length) {
+        slideIndex = 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active');
+    }
+
+    slides[slideIndex - 1].style.display = 'flex';
+    dots[slideIndex - 1].classList.add('active');
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n )
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+prevBtn.addEventListener("click", function onclick() {
+    plusSlides(-1);
+})
+
+nextBtn.addEventListener("click", function onclick() {
+    plusSlides(+1);
+})
+
+dotsArea.addEventListener("click", function onclick(event) {
+    for(let i = 0; i < dots.length + 1; i++) {  
+        if (event.target.classList.contains('slider__tab--item')  && event.target === dots[i - 1]) {
+            currentSlide(i);
+        }
+    }
+})
+
+  setInterval(function(){    
+    currentSlide(slideIndex)
+    slideIndex++;
+    if(slideIndex > slides.length)
+    { slideIndex = 1;  }
+},3000)
+
